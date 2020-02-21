2bdf941df4e54d284833acce8791da9df3ccc84c
Ashish S <ashishjsharda@gmail.com>
2020-02-21 05:12:24 -0500

    Create Hello.groovy

---
+++ Hello.groovy
@@ -0,0 +1,16 @@
+/**
+ * Hello World in Groovy
+ * @author asharda
+ */
+
+class Hello {
+
+    static def hello()
+    {
+        println("Hello World")
+    }
+    static void main(String []args)
+    {
+        hello()
+    }
+}
